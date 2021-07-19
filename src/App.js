1d67726912b2aa9a9d78dcfc6136a6f002b6c24c
rahulKasana <18752429+rahulkasana@users.noreply.github.com>
2021-07-19 21:02:11 +0530

    installation files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="flex h-full w-full bg-gradient-to-r from-purple-400 via-pink-500 to-red-500">
-      <div className='flex'>
-
+    <div className="flex h-full w-full bg-gray-100">
+      <div className="flex w-full h-40 justify-center items-center p-4">
+        <div className="flex h-full w-1/2 rounded-lg hover:border-transparent hover:shadow flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 text-sm font-medium">
+          New Note
+        </div>
       </div>
     </div>
   );
